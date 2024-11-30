8aebbe12f56b6ecdb5c8c1f89de4ea322d785273
Sabari Narayana <sabarinarayana@Sabaris-MacBook-Air.local>
2024-11-30 10:46:06 +0530

    SRP solid added

--- Code/src/ZooSimulation.java
+++ Code/src/ZooSimulation.java
@@ -1,10 +1,7 @@
-
-class ZooSimulation {
+public class ZooSimulation {
     public void simulate(Zoo zoo) {
         System.out.println("Zoo Simulation Running...");
-        System.out.println("Total Animals in Zoo: " + Animal.getTotalAnimals());
         for (Animal animal : zoo.getAnimals()) {
-            System.out.println(animal);
             animal.eat();
             animal.move();
         }
